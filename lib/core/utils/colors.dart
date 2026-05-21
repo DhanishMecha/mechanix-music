3ee47a1e0664e4d1f10ccbe142c5b1c40b684aaa
DhanishMecha <dhanishp@ext.mechasystems.com>
2026-05-21 18:22:18 +0530

    feat: initial app setup and folder architecture

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class MusicColors {
+  static const Color bottomBarBg = Color(0xFF151515);
+  static const Color borderColor = Color(0xFF2D2D2D);
+  static const Color timeLabelColor = Color(0xFF808080);
+  static const Color titleColor = Color(0xFFDDDDDD);
+  static const Color appTitleColor = Color(0xFFADADAD);
+  static const Color placeholderColor = Color(0xFF636363);
+  static const Color searchBarColor = Color(0xFF212121);
+}
